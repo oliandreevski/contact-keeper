8b33e1e294d8c7a24d3a38bcae84566520f81ee7
Oliver <oli.andreevski@gmail.com>
2021-09-11 20:07:31 +0200

    final project

--- server.js
+++ server.js
@@ -1,16 +1,23 @@
 const express = require('express');
+const connectDB = require('./config/db');
 
 const app = express();
 
-app.get('/',(req, res) => 
-res.json({msg:'Welcome to the contact keeper API..'})
+//Connect Database
+connectDB();
+
+//Init Middlewere ???
+app.use(express.json({ extended: false }));
+
+app.get('/', (req, res) =>
+  res.json({ msg: 'Welcome to the contact keeper API..' })
 );
 
 //Define Routes (Sekogas koga ke dojdeme do api/users ili .. ke ja gleda stanata /routes.users ili...)
-app.use('/api/users', require ('./routes/users'));
-app.use('/api/auth', require ('./routes/auth'));
-app.use('/api/contacts', require ('./routes/contacts'));
+app.use('/api/users', require('./routes/users'));
+app.use('/api/auth', require('./routes/auth'));
+app.use('/api/contacts', require('./routes/contacts'));
 
-const PORT=process.env.PORT || 5000;
+const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`))
+app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
